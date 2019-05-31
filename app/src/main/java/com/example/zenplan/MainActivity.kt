f1c1e013d24a5108a3ed8601c4eb2577c81cb1ea
AymericM <aymeric.moehn@gmail.com>
2019-05-31 15:55:59 +0200

    Adding FAB button

--- app/src/main/java/com/example/zenplan/MainActivity.kt
+++ app/src/main/java/com/example/zenplan/MainActivity.kt
@@ -3,29 +3,26 @@ package com.example.zenplan
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v4.app.Fragment
 import android.support.v7.app.ActionBar
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var toolbar: ActionBar
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_profile -> {
-                toolbar.title = "Profile"
                 val songsFragment = ProfileFragment.newInstance()
                 openFragment(songsFragment)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_todo -> {
-                toolbar.title = "Todos"
                 val albumsFragment = TodosFragment.newInstance()
                 openFragment(albumsFragment)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_analytics -> {
-                toolbar.title = "Analytics"
                 val artistsFragment = AnalyticsFragment.newInstance()
                 openFragment(artistsFragment)
                 return@OnNavigationItemSelectedListener true
@@ -51,8 +48,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         openDefaultFragment()
 
+        // FAB ACTION
+
+        val fab: View = findViewById(R.id.fab)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "TODO: Afficher un menu pour créer une todo", Snackbar.LENGTH_LONG)
+                .show()
+        }
+
         // BOTTOM NAVIGATION
-        toolbar = supportActionBar!!
         val bottomNavigation: BottomNavigationView = findViewById(R.id.navigationView)
 
         bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
